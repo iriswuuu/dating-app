5dded6bf9ea6ebae9306f394b0a23084bfe93a24
8128 <iriswnx205@gmail.com>
2023-05-17 04:16:58 -0700

    Update model.py

--- flaskr/model.py
+++ flaskr/model.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import backref
+from sqlalchemy.dialects.postgresql import ARRAY
 
 db = SQLAlchemy()
 
@@ -14,12 +14,12 @@ class User(db.Model):
     username = db.Column(db.String(64), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(120), unique=True)
-    profile = db.relationship("UserProfile", backref="user", uselist=False)
-    matches = db.relationship("Match", backref="users", lazy=True, primaryjoin="User.id==Match.user_id_1")
-    likes_sent = db.relationship("Like", backref="sender", lazy=True, foreign_keys="[Like.user_id_1]", primaryjoin="User.id==Like.user_id_1")
-    likes_received = db.relationship("Like", backref="receiver", lazy=True, foreign_keys="[Like.user_id_2]", primaryjoin="User.id==Like.user_id_1")
-    messages_sent = db.relationship("Message", backref="sender", lazy=True, foreign_keys="[Message.sender_id]", primaryjoin="User.id==Message.sender_id")
-    messages_received = db.relationship("Message", backref="receiver", lazy=True, foreign_keys="[Message.receiver_id]", primaryjoin="User.id==Message.sender_id")
+    profile = db.Column(db.Integer, nullable=True, unique=True)
+    matches = db.Column(ARRAY(db.Integer), nullable=True, unique=True)
+    likes_sent = db.Column(ARRAY(db.Integer), nullable=True, unique=True)
+    likes_received = db.Column(ARRAY(db.Integer), nullable=True, unique=True)
+    messages_sent = db.Column(ARRAY(db.Integer), nullable=True, unique=True)
+    messages_received = db.Column(ARRAY(db.Integer), nullable=True, unique=True)
 
     def __repr__(self):
       return f'<User with id {self.id}>'
@@ -47,10 +47,16 @@ class UserProfile(db.Model):
     __tablename__ = "user_profile"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    photo = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    birthday = db.Column(db.DateTime, nullable=True)
+    gender = db.Column(db.Integer, nullable=True)
+    photo = db.Column(db.String(255), nullable=True)
     description = db.Column(db.String(255))
-    interests = db.Column(db.String(255))
+    interests = db.Column(ARRAY(db.String(255)))
+
+    @classmethod
+    def get_by_id(cls, id):
+      return cls.query.get(id)
 
 
 class Match(db.Model):
@@ -58,8 +64,8 @@ class Match(db.Model):
     __tablename__ = "matches"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id_1 = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    user_id_2 = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user_id_1 = db.Column(db.Integer, nullable=False)
+    user_id_2 = db.Column(db.Integer, nullable=False)
     match_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
@@ -68,9 +74,9 @@ class Like(db.Model):
     __tablename__ = "likes"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id_1 = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    user_id_2 = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    like_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id_1 = db.Column(db.Integer, nullable=False)
+    user_id_2 = db.Column(db.Integer, nullable=False)
+    match_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
 class Message(db.Model):
@@ -78,8 +84,8 @@ class Message(db.Model):
     __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    sender_id = db.Column(db.Integer, nullable=False)
+    receiver_id = db.Column(db.Integer, nullable=False)
     message = db.Column(db.Text, nullable=False)
     send_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
@@ -94,6 +100,9 @@ def connect_to_db(flask_app, db_uri="postgresql:///matchmeet", echo=True):
     db.init_app(flask_app)
 
     print('Connected to the db!')
+
+    with flask_app.app_context():
+      db.create_all()
 
 if __name__ == '__main__':
     from server import app
